cb513c0b6e3c06d473049a5c56ed308b6fe62db0
Dinith Sadeepa Kodithuwakku <dinith.kodithuwakku@gmail.com>
2021-08-24 14:30:47 +0530

    Wallet Controller

--- js/controller/user-wallet-controller.js
+++ js/controller/user-wallet-controller.js
@@ -1,96 +1,93 @@
-function saveCardData() {
-    $.ajax({
-        enctype: 'multipart/form-data',
-        type: 'POST',
-        url: baseUrl + "/api/v1/",
-        processData: false,
-        contentType: false,
-        dataType: "JSON",
-        data: new FormData($('#user_registration_form')[0]),
-        async: true,
-        beforeSend: function () {
-            // show loading
+function onChangePaymentAmount(event) {
+    if (event.value == 0) {
+        $('#paymentGatewayTotalValueId').html(0);
+    } else {
+        $('#paymentGatewayTotalValueId').html(event.value);
+    }
+}
 
-        },
-        complete: function () {
-            // hide loading
+function loadWalletBalance() {
+    if (localStorage.hasOwnProperty("nextbid_login")) {
+        $.ajax({
+            type: 'POST',
+            url: "https://localhost:44395/api/User/GetUser",
+            async: true,
+            data: {
+                Id: parseInt(localStorage.getItem("nextbid_userId"))
+            },
+            beforeSend: function () {
+                // show loading
 
-        },
-        success: function (response) {
-            // handle success
-            if (response.status === 200) {
+            },
+            complete: function () {
+                // hide loading
+
+            },
+            success: function (response) {
+                $('#walletBalanceId').text(response.DepositAmount);
+            },
+            error: function (response) {
+                // handle error
                 Toast.fire({
-                    icon: 'success',
-                    title: 'Successfully saved the card!'
+                    icon: 'error',
+                    title: 'Request failed! cannot preform this action!'
                 })
             }
-        },
-        error: function (response) {
-            // handle error
-            Toast.fire({
-                icon: 'error',
-                title: 'Request failed! cannot preform this action!'
-            })
-        }
-    });
-}
-
-$('form#addCardForm').on('submit', e => {
-    e.preventDefault();
-    e.stopPropagation();
-    if (document.querySelector('#addCardForm').checkValidity()) {
-        // save card data
-        saveCardData();
-    } else {
-        Toast.fire({
-            icon: 'error',
-            title: 'Invalid user credentials! Please check again.'
-        })
+        });
     }
-});
-
-function _createTransactionRow() {
-
-}
-
-function createAndAppendTransactionsList(resp) {
-
-}
-
-function loadWalettHistory() {
-
-    $.ajax({
-        enctype: 'multipart/form-data',
-        type: 'GET',
-        url: baseUrl + "/api/v1/",
-        processData: false,
-        contentType: false,
-        dataType: "JSON",
-        async: true,
-        beforeSend: function () {
-            // show loading
-
-        },
-        complete: function () {
-            // hide loading
-
-        },
-        success: function (response) {
-            // handle success
-            if (response.status === 200) {
-                createAndAppendTransactionsList(response.data);
-            }
-        },
-        error: function (response) {
-            // handle error
-            Toast.fire({
-                icon: 'error',
-                title: 'Request failed! cannot preform this action!'
-            })
-        }
-    });
 }
 
 $(document).ready(function () {
-    loadWalettHistory();
+    loadWalletBalance();
 });
+
+$("#user_topup_payment_form").on("submit", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (document.querySelector("#user_topup_payment_form").checkValidity()) {
+        confirmTopUpWallet();
+    } else {
+        Toast.fire({
+            icon: "error",
+            title: "Invalid Data! Please check again.",
+        });
+    }
+});
+
+function confirmTopUpWallet() {
+    if (localStorage.hasOwnProperty("nextbid_login")) {
+        $.ajax({
+            type: 'POST',
+            url: "https://localhost:44395/api/User/Wallet/TopUp",
+            data: {
+                UserId: parseInt(localStorage.getItem("nextbid_userId")),
+                DepositAmount: parseFloat($('input[name=depositAmountInput]').val())
+            },
+            async: true,
+            success: function (response) {
+                Toast.fire({
+                    icon: "success",
+                    title: "User wallet balance updated!",
+                });
+                $("input[type=text], textarea, input[type=number]").val("");
+                $('#paymentGatewayTotalValueId').html(0);
+
+
+                window.location.href = baseUrl + 'user-wallet.html';
+            },
+            error: function (response) {
+
+                if (response.status === 500) {
+                    Toast.fire({
+                        icon: 'error',
+                        title: 'Request failed! cannot preform this action!'
+                    })
+                }
+                // handle error
+
+            }
+        });
+    } else {
+        checkLogins();
+    }
+}
