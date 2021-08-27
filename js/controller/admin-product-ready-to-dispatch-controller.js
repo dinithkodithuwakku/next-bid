3570cefb2fcdc2c89d677c2ef11c150aedfbba04
Dinith Sadeepa Kodithuwakku <dinith.kodithuwakku@gmail.com>
2021-08-27 19:57:43 +0530

    Product Dispatch UI

---
+++ js/controller/admin-product-ready-to-dispatch-controller.js
@@ -0,0 +1,67 @@
+const tableBody = $('#productReadyToDispatchTable tbody');
+
+function loadReadyToDispatchProducts() {
+    $.ajax({
+        type: 'GET',
+        url: "https://localhost:44395/api/Item/GetReadyToDispatchList",
+        async: true,
+        beforeSend: function () {
+            // show loading
+        },
+        complete: function () {
+            // hide loading
+        },
+        success: function (response) {
+            console.log(response);
+            tableBody.empty();
+            for (let product of response) {
+                console.log("product : ", product);
+                tableBody.append(_createProductTableRow(product));
+
+                let dispatchButton = document.createElement('button');
+                dispatchButton.className = "btn btn-sm btn-primary border-0";
+                dispatchButton.innerHTML = "Dispatch";
+                dispatchButton.addEventListener('click', function () {
+
+                });
+
+                document.getElementById(product.Item.ItemId).appendChild(dispatchButton);
+
+            }
+
+        },
+        error: function (response) {
+            // handle error
+            Toast.fire({
+                icon: 'error',
+                title: 'Request failed! cannot preform this action!'
+            })
+        }
+    });
+}
+
+function _createProductTableRow(product) {
+    return `
+        <tr>
+            <th class="fw-normal" scope="row">${product.Item.ItemId}</th>
+            <td class="fw-bold">${product.Item.ItemName}</td>
+            <td>${product.Item.ItemDiscription}</td>
+            <td><a class="badge bg-dark text-decoration-none">${product.ItemBidding.HighestBid}</a></td>
+            <td><a class="badge bg-dark text-decoration-none">${product.User.FirstName} ${product.User.LastName}</a></td>
+            <td><a class="badge bg-dark text-decoration-none">${product.User.ContactNumber}</a></td>
+            <td><a class="badge bg-dark text-decoration-none">${product.User.Address} ${product.User.City}</a></td>
+            
+            <td>
+                <!--<a class="btn btn-sm btn-dark border-0 me-1" href="admin-customer-view.html"-->
+                   <!--title="View Details">-->
+                    <!--View-->
+                <!--</a>-->
+                 <span id=${product.Item.ItemId}></span>
+            </td>
+        </tr>
+    `;
+}
+
+$(document).ready(function () {
+    loadReadyToDispatchProducts();
+});
